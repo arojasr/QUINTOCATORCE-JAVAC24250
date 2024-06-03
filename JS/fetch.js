ca835d3b4c02d11d9bf705fa1c521fc5e676925a
belucham2323 <belentrujillo2794@gmail.com>
2024-06-02 21:00:25 -0300

    add new url to fech

--- JS/fetch.js
+++ JS/fetch.js
@@ -16,8 +16,34 @@ console.log(datos.title, datos.image, datos.price, datos.category);
 })
 */
 // Declaramos una función que renderiza elementos en el DOM
+function eliminar_caracter(texto,caracter){
+    let tiene=true;
+    while(tiene==true){
+        if(texto.indexOf(caracter)!=-1){
+            texto=texto.replace(caracter,"");
+        }else tiene=false;
+    }
+    return texto;
+}
+function eliminar_lista_caracteres(texto,lista){
+    for (let i = 0; i < lista.length; i++) {
+        texto=eliminar_caracter(texto,lista[i]);
+    }
+    return texto;
+}
+function checkIfHasJpg(url){
+    let texto=url.split(".");
+    const extension=texto[texto.length-1];
+    console.log(extension);
+    if(extension=="jpg"||extension=="jpeg"||extension=="png"||extension=="jift"||extension=="webp"){
+        // console.log("verdad--------------------------------");
+        return true;
+    }
+    return false;
+}
 function pedirDatos(){
-    fetch("https://fakestoreapi.com/products")
+    // fetch("https://fakestoreapi.com/products")
+    fetch("https://api.escuelajs.co/api/v1/products")
     .then(function(respuesta){
         return respuesta.json();
     })
@@ -27,22 +53,31 @@ function pedirDatos(){
 
         // Utilizamos un for-each
         productos.forEach(function(dato){
-            //Creamos el elemento html
-            const article = document.createElement('div');
-            //Agregamos estilo a article
-            article.classList.add("product");
-            //Inyectamos contenido a <article>
-            article.innerHTML = `
-            <img src="${dato.image}" alt="Imagen del producto ${dato.title}" class="product_img" width="300" height="300">
-            <div class="product_description">
-                    <h3 class="title_product">${dato.title}</h3>
-                    <h2 class="title_category">Categoria:${dato.category}</h3>
-                    <span class="price_product">${dato.price}</span>
-            </div>
-            <i class="product_icon fa-solid fa-cart-plus"></i>
-            `
-            //Adjuntamos el contenido 
-            contenedor.appendChild(article);
+
+            let texto=dato.images[0];
+            texto=eliminar_lista_caracteres(texto,["'",'"',"[","]"]);
+            console.log(texto);
+            if(checkIfHasJpg(texto)){
+                //Creamos el elemento html
+                const article = document.createElement('div');
+                //Agregamos estilo a article
+                article.classList.add("product");
+                //Inyectamos contenido a <article>
+                // console.log(dato);
+                // <img src="${dato.image}" alt="Imagen del producto ${dato.title}" class="product_img" width="300" height="300">
+                // <h2 class="title_category">Categoria:${dato.category}</h3>
+                article.innerHTML = `
+                <img src="${texto}" alt="Imagen del producto ${dato.title}" class="product_img" width="300" height="300">
+                <div class="product_description">
+                        <h3 class="title_product">${dato.title}</h3>
+                        <h2 class="title_category">Categoria:${dato.category.name}</h3>
+                        <span class="price_product">${dato.price}</span>
+                </div>
+                <i class="product_icon fa-solid fa-cart-plus"></i>
+                `
+                //Adjuntamos el contenido 
+                contenedor.appendChild(article);    
+            }
         })
 
     })
